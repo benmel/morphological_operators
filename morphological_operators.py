e63659ed77d2c540218003a204c8e536e4fea113
Ben Meline <benjaminmeline2015@u.northwestern.edu>
2014-10-14 23:46:56 -0500

    Comments

--- morphological_operators.py
+++ morphological_operators.py
@@ -22,13 +22,16 @@ class MorphologicalOperators:
 			for j in xrange(self.cols):
 				current = self.labeled_image.get_pixel(i,j)
 				new = Pixel(current.label, current.row, current.col)
+				"""Test if pixel is foreground"""
 				if (current.is_not_label(self.background)):
+					"""Get neighboring pixels"""
 					coords = self.se.get_coords(current.row, current.col)
 					neighbors = self.labeled_image.get_pixels(coords)
 					other_bg_px = False
 					for n in neighbors:
 						if n.is_label(self.background):
 							other_bg_px = True
+					"""If a neighbor is background set label to background"""
 					if other_bg_px and len(neighbors) > 0:
 						new.label = self.background
 				new_image.label_pixel(new)		
@@ -40,27 +43,33 @@ class MorphologicalOperators:
 			for j in xrange(self.cols):
 				current = self.labeled_image.get_pixel(i,j)
 				new = Pixel(current.label, current.row, current.col)
+				"""Test if pixel is background"""
 				if (current.is_label(self.background)):
+					"""Get neighboring pixels"""
 					coords = self.se.get_coords(current.row, current.col)
 					neighbors = self.labeled_image.get_pixels(coords)
 					other_fg_px = False
 					for n in neighbors:
 						if n.is_not_label(self.background):
 							other_fg_px = True
+					"""If a neighbor is foreground set label to foreground"""
 					if other_fg_px and len(neighbors) > 0:
 						new.label = self.foreground
 				new_image.label_pixel(new)
 		self.labeled_image = deepcopy(new_image)	
 
 	def opening(self):
+		"""Erosion then dilation"""
 		self.erosion()
 		self.dilation()
 
 	def closing(self):
+		"""Dilation then erosion"""
 		self.dilation()
 		self.erosion()
 
 	def boundary(self):
+		"""Subtract erosion from original"""
 		self.erosion()
 		boundary = np.subtract(self.original_image.matrix, self.labeled_image.matrix)
 		self.labeled_image.matrix = deepcopy(boundary)
@@ -72,6 +81,7 @@ class MorphologicalOperators:
 		self.labeled_image.save(output_file)	
 
 	def save_text(self):
+		"""Save matrix in csv file for debugging"""
 		np.savetxt('in.csv', self.original_image.matrix, delimiter=',', fmt='%d')
 		np.savetxt('out.csv', self.labeled_image.matrix, delimiter=',', fmt='%d')	
 	
@@ -93,6 +103,7 @@ class LabeledImage:
 		return Pixel(self.matrix.item(row,col), row, col)
 
 	def get_pixels(self, coords):
+		"""Return all pixels specified by coordinates array"""
 		pixels = []
 		row_max, col_max = self.shape()
 		for c in coords:
@@ -142,14 +153,17 @@ class Pixel:
 
 class StructuringElement:
 	def __init__(self, se):
+		"""Create structuring element"""
 		self.matrix = se
 		self.rows,self.cols = self.matrix.shape
 		if (self.rows % 2 == 0) or (self.cols % 2 == 0):
 			print 'Dimensions of structuring element must be odd'
 			sys.exit()
+		"""Get origin of matrix"""	
 		self.origin = Pixel(None, self.rows/2, self.cols/2)
 
 	def get_coords(self, row, col):
+		""""Get coordinates of surrounding pixel relative to origin"""
 		coords = []
 		for i in xrange(self.rows):
 			for j in xrange(self.cols):
@@ -159,6 +173,7 @@ class StructuringElement:
 					col_dist = current.col - self.origin.col
 					new_row = row + row_dist
 					new_col = col + col_dist
+					"""Skip origin"""
 					if not(row_dist == 0 and col_dist == 0):
 						coords.append([new_row,new_col])
 		return coords
@@ -196,16 +211,19 @@ def main():
 		elif opt in ("-o", "--outputf"):
 			outputf = arg
 
+	"""Required arguments"""
 	if not morph_op or not se_in or not inputf:
 		usage()
 		sys.exit()
 
+	"""Possible morphological operators"""	
 	if morph_op not in ("erosion", "dilation", "opening", "closing", "boundary", 
 											"closing_boundary", "closing_opening", "closing_opening_boundary"):
 		print 'Available morphological operators are erosion, dilation, opening, closing, '\
 					'boundary, closing_boundary, closing_opening and closing_opening_boundary'
 		sys.exit()				
 
+	"""Possible structuring elements"""
 	if se_in == 'rect3x3':
 		se = np.ones((3,3), dtype=np.int)		 		
 	elif se_in == 'rect5x5':
@@ -264,6 +282,7 @@ def main():
 	img = cv2.imread(inputf, 0)
 	mo = MorphologicalOperators(img, se)
 
+	"""Call morphological operators"""
 	if morph_op == "erosion":
 		mo.erosion()
 	elif morph_op == "dilation":
@@ -292,6 +311,7 @@ def main():
 		mo.original_image = deepcopy(mo.labeled_image)
 		mo.boundary()	
 
+	"""Save or plot image"""	
 	if outputf:
 		mo.save(outputf)
 	else:
